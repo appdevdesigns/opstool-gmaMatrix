f6628b1fe505a0f0488ea897fba383a882fec230
D. Shun-Luoi Fong <dsf@shunluoifong.com>
2014-08-06 15:51:26 -0700

    adjustments to gmaMatrix UI

--- assets/opstools/GMAMatrix/setup.js
+++ assets/opstools/GMAMatrix/setup.js
@@ -3,6 +3,7 @@ steal(
         '//opstools/GMAMatrix/controllers/GMAMatrix.js',
         '//opstools/GMAMatrix/matrix.css',
         '//opstools/GMAMatrix/matrix-scratch.css',
+		'js/dropzone.min.js',
         '/site/labels/opstools/GMAMatrix'
 ).then(function(){
 
