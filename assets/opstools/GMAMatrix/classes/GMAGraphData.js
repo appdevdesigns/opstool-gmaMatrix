2d7a075a3c8b0bb4e707e5a2c60c35a802388d0a
Joshua Chan <joshua@appdevdesigns.net>
2014-10-24 03:20:25 -0700

    Added objects for parsing data and rendering graphs

---
+++ assets/opstools/GMAMatrix/classes/GMAGraphData.js
@@ -0,0 +1,172 @@
+
+steal(
+        // List your Controller's dependencies here:
+        'appdev',
+        //'opstools/GMAMatrix/classes/GMAMeasurement.js',
+        //'opstools/GMAMatrix/classes/GMAPlacement.js',
+function(){
+
+    // Namespacing conventions:
+    // AD.classes.[application].[Class]  --> Object
+    if (typeof AD.classes.gmamatrix == 'undefined') AD.classes.gmamatrix = {};
+    AD.classes.gmamatrix.GMAGraphData = can.Construct.extend({
+
+    },{
+
+        init: function( data ) {
+            data = AD.defaults({
+                nodeId: -1,
+                startDate: 'yyyy-mm-dd',
+                endDate: 'yyyy-mm-dd'
+            }, data);
+
+            for (var d in data) {
+                this[d] = data[d];
+            }
+            
+            this.graphData = null;
+            this.placementData = null;
+            this.dataReady = $.when(this.fetchData(), this.fetchPlacements());
+        },
+
+
+        fetchData: function() {
+            var dfd = AD.sal.Deferred();
+            var self = this;
+            
+            AD.comm.service.get({ 
+                url: '/opstool-gmaMatrix/gmamatrix/graphData', 
+                data: { 
+                    nodeId: this.nodeId,
+                    startDate: this.startDate,
+                    endDate: this.endDate
+                }
+            })
+            .fail(function(err){
+                if (err.message.match(/Invalid PGTIOU/)) {
+                    AD.comm.hub.publish('ad.auth.reauthenticate', {});
+                }
+                dfd.reject(err);
+            })
+            .done(function(data){
+                self.graphData = data;
+                dfd.resolve();
+            });
+
+            return dfd;
+        },
+        
+        
+        
+        // Get the placement data for the GMA node currently loaded.
+        fetchPlacements: function() {
+            var dfd = AD.sal.Deferred();
+            var self = this;
+            
+            AD.comm.service.get({
+                url: '/opstool-gmaMatrix/gmamatrix/placements',
+                data: {
+                    nodeId: self.nodeId
+                }
+            })
+            .fail(function(err){
+                dfd.reject(err);
+            })
+            .done(function(list){
+                self.placementData = {
+                /*
+                    <measurementId>: { type: 'staff', key: 'etmm' },
+                    <measurementId>: { type: 'disciple', key: 'igc' },
+                    ...
+                */
+                };
+                for (var i=0; i<list.length; i++) {
+                    self.placementData[ list[i].measurement_id ] = {
+                        key: list[i].location,
+                        type: list[i].type
+                    };
+                }
+                dfd.resolve(self.placementData);
+            });
+            
+            return dfd;
+        },
+        
+        
+        // Returns a basic object lookup of strategyID:strategyName.
+        // fetchData() must have completed before this.
+        strategies: function(){
+            var strategies = {};
+            if (this.graphData) {
+                for (var i=0; i<this.graphData.strategies.length; i++) {
+                    var strategyID = this.graphData.strategies[i].id;
+                    var strategyName = this.graphData.strategies[i].name;
+                    strategies[strategyID] = strategyName;
+                }
+            }
+            return strategies;
+        },
+        
+        
+        // fetchData() and fetchPlacements() must have been completed before this.
+        dataForStrategy: function(strategyID) {
+            var self = this;
+            var results = {
+                periods: [],
+                staffLMIs: {},
+                discipleLMIs: {}
+            };
+            
+            // Package the graph and placement data for the given strategy
+            results.periods = self.graphData.periods;
+            for (var i=0; i<self.graphData.measurements.length; i++) {
+                var measurementID = self.graphData.measurements[i].id;
+                
+                // Only proceed if there is placement data for this measurement
+                if (self.placementData[measurementID]) {
+                    var type = self.placementData[measurementID].type;
+                    var keyLMI = self.placementData[measurementID].key;
+                    var values = self.graphData.measurements[i].values;
+    
+                    var groupKey;
+                    if (type == 'disciple') {
+                        groupKey = 'discipleLMIs';
+                    } else {
+                        groupKey = 'staffLMIs';
+                    }
+                    
+                    if (strategyID == self.graphData.measurements[i].strategyId) {
+                        if (!results[groupKey][keyLMI]) {
+                            // Use initial measurement values
+                            results[groupKey][keyLMI] = values;
+                        } else {
+                            // Subsequent measurements within the same LMI are
+                            // added on to the previous ones.
+                            for (var j=0; j<results[groupKey][keyLMI].length; j++) {
+                                results[groupKey][keyLMI][j] += values[j];
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return results;
+        },
+        
+        
+        
+        getNodeID: function(){
+            return this.nodeId;
+        },
+
+
+
+        label: function() {
+            return this.graphData.title;
+        }
+
+
+    });
+
+
+});
---
+++ assets/opstools/GMAMatrix/controllers/LMIGraph.js
@@ -0,0 +1,157 @@
+
+steal(
+        // List your Controller's dependencies here:
+        'appdev',
+//        'opstools/GMAMatrix/models/Projects.js',
+//        'appdev/widgets/ad_delete_ios/ad_delete_ios.js',
+//        'opstools/GMAMatrix/views/LMIDefinition/LMIDefinition.ejs',
+function(){
+
+    // Namespacing conventions:
+    // AD.controllers.[application].[controller]
+    if (typeof AD.controllers.opstools == 'undefined') AD.controllers.opstools = {};
+    if (typeof AD.controllers.opstools.GMAMatrix == 'undefined') AD.controllers.opstools.GMAMatrix = {};
+    AD.controllers.opstools.GMAMatrix.LMIGraph = AD.classes.UIController.extend({
+        
+        // callback function used for graph tooltips
+        tooltipCB: function() {
+            return this.period + ': ' + this.score;
+        },
+        
+        // graph color?
+        seriesStyles: [
+            {
+                fill: "#999999",
+                stroke: "#333333"
+            }
+        ],
+        
+        // Recursively compute the greatest common denominator of a & b
+        // @param integer a
+        // @param integer b
+        // @return integer
+        gcd: function(a, b) {
+            if (!b) {
+                return a;
+            }
+            return gcd(b, a % b);
+        },
+        
+        // @param integer a
+        // @param integer b
+        // @return string
+        ratioInteger: function(a, b) {
+            var gcd = this.gcd(a, b);
+            return '' + (a/gcd) + ':' + (b/gcd);
+        },
+        
+        // @param integer a
+        // @param integer b
+        // @return string
+        ratioFloat: function(a, b) {
+            if (a * b == 0) {
+                return '';
+            }
+            else if (b > a) {
+                return '1:' + (Math.round(b/a * 100) / 100);
+            } else {
+                return '' + (Math.round(a/b * 100) / 100) + ':1';
+            }
+        }
+    
+    }, {
+
+        init: function (element, options) {
+            var self = this;
+            options =  AD.defaults({
+                templateDOM: '//opstools/GMAMatrix/views/LMIGraph/LMIGraph.ejs',
+                titleLMI: '', // e.g. 'Exposing through mass means'
+                key: '' // e.g. 'etmm'
+            }, options);
+            this.options = options;
+
+            // Call parent init
+            AD.classes.UIController.apply(this, arguments);
+
+
+            this.initDOM();
+        },
+
+        
+        initDOM: function () {
+            var html = can.view(this.options.templateDOM, { title: this.options.titleLMI } );
+            this.element.html(html);
+        },
+        
+
+        // @param object data
+        // {
+        //      staff: [ val1, val2, val3, ... ],
+        //      disciple: [ val1, val2, val3, ... ],
+        //      periods: [ date1, date2, date2, ... ]
+        // }
+        render: function (data) {
+            var $staffGraph = this.element.find('.staff .gmaSparkline');
+            var $staffTotal = this.element.find('.staff .field-total');
+            var $discipleGraph = this.element.find('.disciple .gmaSparkline');
+            var $discipleTotal = this.element.find('.disciple .field-total');
+            var $ratio = this.element.find('.total .field-ratio');
+            var $combinedTotal = this.element.find('.total .field-total');
+            
+            var staffData = [];
+            var discipleData = [];
+            var staffTotal = 0;
+            var discipleTotal = 0;
+            
+            var parseValue = function(index, dataArray) {
+                if (Array.isArray(dataArray)) {
+                    return parseInt( dataArray[index] ) || 0;
+                } else {
+                    return 0;
+                }
+            };
+            
+            for (var i=0; i<data.periods.length; i++) {
+                var staffValue = parseValue(i, data.staff);
+                staffTotal += staffValue;
+                staffData.push({ 
+                    period: data.periods[i],
+                    score: staffValue
+                });
+                var discipleValue = parseValue(i, data.disciple);
+                discipleTotal += discipleValue;
+                discipleData.push({
+                    period: data.periods[i],
+                    score: discipleValue
+                });
+            }
+            
+            $staffGraph.wijsparkline({
+                data: staffData,
+                bind: 'score',
+                tooltipContent: this.constructor.tooltipCB,
+                type: 'area',
+                seriesStyles: this.constructor.seriesStyles
+            });
+            $staffTotal.text(staffTotal);
+
+            $discipleGraph.wijsparkline({
+                data: discipleData,
+                bind: 'score',
+                tooltipContent: this.constructor.tooltipCB,
+                type: 'area',
+                seriesStyles: this.constructor.seriesStyles
+            });
+            $discipleTotal.text(discipleTotal);
+            
+            $ratio.text(this.constructor.ratioFloat(staffTotal, discipleTotal));
+            $combinedTotal.text(staffTotal + discipleTotal);
+        
+        }
+
+
+
+    });
+
+
+});
